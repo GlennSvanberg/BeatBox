59cc86bff5dfdd60dde7089bd503123970148c06
Glenn Svanberg <signeratsvanberg@gmail.com>
2019-08-19 23:11:31 +0200

    Chapter 20

---
+++ app/src/main/java/com/svanberggroup/beatbox/SoundViewModel.java
@@ -0,0 +1,25 @@
+package com.svanberggroup.beatbox;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+    }
+}
